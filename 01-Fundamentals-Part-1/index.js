a0cd181eb0fe72229edf5ddd92c98929f49a4cbf
Lypzis <victor.piccoli@pm.me>
2024-07-10 22:16:40 -0300

    update

---
+++ 01-Fundamentals-Part-1/index.js
@@ -0,0 +1,31 @@
+// console.log("I'm alive! :D");
+
+// const country = "Brazil";
+// const continent = "South America";
+// let population = 220000000;
+
+// const isIsland = false;
+// const language = "portuguese";
+
+// population = 1;
+// console.log(country, population, language, isIsland);
+
+/* const markWeight = 78;
+const markHeight = 1.69;
+
+const johnWeight = 95;
+const johnHeight = 1.88;
+
+// mass is in Kg and height in Meters
+const calcBMI = (height, weight) => {
+  return weight / height ** 2;
+};
+
+const BMIMark = calcBMI(markHeight, markWeight);
+const BMIJohn = calcBMI(johnHeight, johnWeight);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark);
+console.log(BMIJohn);
+console.log(markHigherBMI); */
